001e9a87092db0f13932d26c6545bf9bc3225021
maxchou <callmegto@hotmail.com>
2017-03-09 18:22:43 +0800

    DONE

---
+++ MyProject/Clientside/App.ts
@@ -0,0 +1,9 @@
+﻿import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+class App extends Vue {
+    name = "Max";
+}
+
+new App({ el: "#app" });
---
+++ MyProject/webpack.config.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var path = require('path');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    module: {
+        loaders: [
+            // This example only configures Webpack to load .ts files. You can also drop in loaders
+            // for other file types, e.g., .coffee, .sass, .jsx, ...
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    },
+    entry: {
+        main: ['./Clientside/App.ts']
+    },
+    output: {
+        path: path.join(__dirname, 'wwwroot', 'dist'),
+        filename: '[name].js',
+        publicPath: '/dist/'
+    }
+};
